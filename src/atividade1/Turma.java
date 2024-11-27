338be5eed283bec2eac5a283419549eda118ad41
joao-06 <66530247+joao-06@users.noreply.github.com>
2024-11-27 14:41:07 -0300

    Update Turma.java

--- src/atividade1/Turma.java
+++ src/atividade1/Turma.java
@@ -60,29 +60,49 @@ class Turma{
         for (Estudante estudante : estudantes) {
             if (estudante.getMatricula().equals(matricula)) {
                 System.out.println("Situação do aluno " + estudante.getNome() + " (" + matricula + "): ");
-                estudante.mostrarDadosEstudante();  // Exibe os dados e a situação
+                estudante.mostrarDadosEstudante();
                 return;
             }
         }
         System.out.println("Aluno com matrícula " + matricula + " não encontrado na turma.");
     }
     
-    public void listarEstudantes() {
-        System.out.println("Estudantes na turma " + identificacao + ":");
+
+        public void classificarEstudantes() {
+        List<Estudante> aprovados = new ArrayList<>();
+        List<Estudante> finais = new ArrayList<>();
+        List<Estudante> reprovados = new ArrayList<>();
+
         for (Estudante estudante : estudantes) {
+            String situacao = estudante.getSituacao();
+            if (situacao.equals("Aprovado")) {
+                aprovados.add(estudante);
+            } else if (situacao.equals("Final")) {
+                finais.add(estudante);
+            } else if (situacao.equals("Reprovado")) {
+                reprovados.add(estudante);
+            }
+        }
+
+       // Exibindo os resultados
+        System.out.println("\n Alunos Aprovados");
+        for (Estudante estudante : aprovados) {
+            System.out.println("Estudantes na turma " + identificacao + ":");
             estudante.mostrarDadosEstudante();
         }
-        
-    }
 
-    public void mostrarDadosTurma() {
-        System.out.println("Identificacao: " + identificacao);
-        mostrarDadosProfessor();
-        mostrarDadosEstudantes();
-        
-    }
+        System.out.println("\n Alunos em Final");
+        for (Estudante estudante : finais) {
+            System.out.println("Estudantes na turma " + identificacao + ":");
+            estudante.mostrarDadosEstudante();
+        }
 
-    
+        System.out.println("\n Alunos Reprovados");
+            for (Estudante estudante : reprovados) {
+            System.out.println("Estudantes na turma " + identificacao + ":");
+            estudante.mostrarDadosEstudante();
+        }
+            
      public void listarEstudantes() {
         System.out.println("Estudantes na turma " + identificacao + ":");
         for (Estudante estudante : estudantes) {
