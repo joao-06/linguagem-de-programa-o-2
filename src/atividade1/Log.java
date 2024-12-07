6a67daaf4a383ad7a4509163b1eec365e5417311
joao-06 <66530247+joao-06@users.noreply.github.com>
2024-12-07 09:10:51 -0300

    Update Log.java

--- src/atividade1/Log.java
+++ src/atividade1/Log.java
@@ -1,12 +1,52 @@
-public class Log {
-    private String acao;
-    private String informacao;
+import java.time.LocalDateTime;
 
-    public Log(String acao, String informacao) {
+public class Log {
+    private String usuario;
+    private String acao;
+    private LocalDateTime data;
+    private LocalDateTime hora;
+
+    public Log(String usuario, String acao) {
+        this.usuario = usuario;
         this.acao = acao;
-        this.informacao = informacao;
+        this.data = LocalDateTime.now();
+        this.hora = LocalDateTime.now();
     }
-    public void printLog() {
-        System.out.println(acao + ": " + informacao);
-    } 
+
+    @Override
+    public String toString() {
+        return Log ["Usuario=" + usuario + , "Acao=" + acao + , "Data=" + data + , "Hora=" +hora+];
+    }
+
+    // Getters e Setters (opcionais)
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getAcao() {
+        return acao;
+    }
+
+    public void setAcao(String acao) {
+        this.acao = acao;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setDataHora(LocalDateTime data) {
+        this.dataHora = data;
+    }
+
+    public LocalDateTime getHora() {
+        return hora;
+    }
+
+    public void setDataHora(LocalDateTime hora) {
+        this.dataHora = hora;
 }
