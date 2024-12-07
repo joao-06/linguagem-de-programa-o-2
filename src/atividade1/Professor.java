dbd1a85c9e1d9633ea4458fd29e2a929a6edc57e
joao-06 <66530247+joao-06@users.noreply.github.com>
2024-12-07 09:46:01 -0300

    Update Professor.java

--- src/atividade1/Professor.java
+++ src/atividade1/Professor.java
@@ -9,12 +9,18 @@ package atividade1;
  *
  * @author aluno
  */
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Professor extends Pessoa {
     private String siaep;
+    private List<Log> logs;
 
     public Professor(String nome, String cpf, String endereco, String telefone, String siaep) {
         super(nome, cpf, endereco, telefone); 
         this.siaep = siaep;
+        this.logs = new ArrayList<>();
     }
 
 
@@ -24,6 +30,34 @@ class Professor extends Pessoa {
 
     public void setSiaep(String siaep) {
         this.siaep = siaep;
+    }
+    public void alterarNota(Estudante estudante, double novaNota, String usuario) {
+       
+        double antigaNota = estudante.getNota1(); // Alterar a primeira nota
+
+        // Altera a nota do estudante
+        estudante.setNota1((float) novaNota);  // Atualizando a primeira nota do aluno
+        estudante.calcularMedia(); // Recalcula a média do aluno após a alteração
+
+        // Criar o log da alteração
+        String acao = "Alteração de nota de " + antigaNota + " para " + novaNota + " no aluno " + estudante.getNome();
+        Log log = new Log(usuario, acao);
+
+        // Adiciona o log à lista de logs do professor
+        logs.add(log);
+
+        // Adiciona o log à lista de logs do aluno
+        estudante.getLogs().add(log);
+
+        System.out.println("Nota alterada e log registrado com sucesso.");
+    }
+
+   // Método para exibir os logs do professor
+    public void exibirLogs() {
+        System.out.println("Logs do professor " + this.getNome() + ":");
+        for (Log log : logs) {
+            System.out.println(log);
+        }
     }
 
     public void mostrarDadosProfessor() {
